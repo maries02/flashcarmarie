e8b29ad9a050d750b4cefa273d7401a52a719755
maries02 <148551399+maries02@users.noreply.github.com>
2024-11-01 11:30:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Palmeiras',
+    'Como é o nome completo do Palmeiras?',
+    'Sociedade Esportiva Palmeiras'
+)
+
+criaCartao(
+    'Palmeiras',
+    'Qual foi o ano do Fundamento do Palestra Itália?',
+    '1914'
+)
+
+criaCartao(
+    'Palmeiras',
+    'Quantos brasileirões nós temos? (Desde 1937)',
+    '12 brasileirões'
+)
+
+criaCartao(
+    'Palmeiras',
+    ' Nosso maior Rival? (Torcida Organizada)',
+    'Gaviões da fiel'
+)
